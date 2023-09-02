dd5e0e0f1c489dee1c74cfc1395383ee09736353
Ali <109287752+xunzag@users.noreply.github.com>
2023-09-02 14:52:27 +0000

    Updated Portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a Software Engineer student with a background in MERN stack development and Data Analytics. I am passionate about solving problems in new creative ways to drive innovation. By leveraging my experience I continually look for new and better ways to make tech accessible by all. ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Data Analytics",
+  "AI/ML",
+  "MERN Stack",
+  "Mobile development",
+  "Public Speaking"  
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate and motivated about learning new technologies and solving problems. I am a team player and enjoy working with others to achieve a common goal. I am a quick learner and enjoy taking on new challenges.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "A Portal to make friends or connection in University.",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is a MERN stack application. It uses React on the front end, Node.js and on the back end, and MongoDB for the database.",
+    url: "https://github.com/xunzag/uit-hub",
   },
   {
-    title: "Web Development for Beginners",
+    title: "An experimental project to generate voice from text using GPT-3.",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A python program that uses GPT-3 to generate voice from text.",
+    url: "https://github.com/xunzag/voicegpt",
   },
   {
-    title: "My Resume Site",
+    title: "A Scheduler Web-App",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A web application that allows users to experience Scheduling Algorithms with threads and processes.",
+    url: "https://salad-scheduler.netlify.app",
   },
 ];
 
